59e40ab95b9a4a5b828b3a72178c6d236ffac22f
Brian <wueso19@admin.com>
2021-10-26 17:08:24 -0300

    Formulario Contacto

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// js para el menu desplegable
+let MenuItems = document.getElementById("MenuItems");
+MenuItems.style.maxHeight = "0px";
+
+function menutoggle() {
+  if (MenuItems.style.maxHeight == "0px") {
+    MenuItems.style.maxHeight = "200px";
+  } else {
+    MenuItems.style.maxHeight = "0px";
+  }
+}
