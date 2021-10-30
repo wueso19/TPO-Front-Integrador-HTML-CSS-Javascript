043e7b90076dfb18e27bb644e502cc95d2829e7f
Brian <wueso19@admin.com>
2021-10-30 19:21:28 -0300

    Acualizaciones varias

--- js/script.js
+++ js/script.js
@@ -9,3 +9,25 @@ function menutoggle() {
     MenuItems.style.maxHeight = "0px";
   }
 }
+
+// redireccionar al hacer click en un producto
+function detail() {
+  window.location.href = ("product-details.html")
+}
+
+// galeria de imagenes para 1 producto
+let productImg = document.getElementById("productImg")
+let smallImg = document.getElementsByClassName("small-img")
+
+smallImg[0].onclick = function(){
+  productImg.src = smallImg[0].src
+}
+smallImg[1].onclick = function(){
+  productImg.src = smallImg[1].src
+}
+smallImg[2].onclick = function(){
+  productImg.src = smallImg[2].src
+}
+smallImg[3].onclick = function(){
+  productImg.src = smallImg[3].src
+}
